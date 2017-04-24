7f6daf3b33ac92b0f2e13702d7f55f4d9535302c
ScottHosking <jshosking@gmail.com>
2017-04-24 15:00:44 +0100

    use date parser

--- erai.py
+++ erai.py
@@ -8,25 +8,50 @@ import os.path
 import cf_units as units
 import iris
 
+
 ### Create folder for storing data
 baspy_path = os.path.expanduser("~/.baspy")
 if not os.path.exists(baspy_path):
 	os.makedirs(os.path.expanduser(baspy_path))
 
 
-def get_6hr_fnames(start_date, end_date, level_str):
+def get_6hr_fnames(start_date, end_date, var_name, months='all'):
 	"""
+
 	Get /Path/filenames for ERA-Interim files within date range
-	level_str for JASMIN:
-		'as' surface variables
-		'ap' pressure level variables
+
+	Start and end date can be specified using a range of strings:
+		e.g., '19790101', '1979-01-01', '19790101010101'
+
+	Months, default is all months
+		e.g., Months=[6,7,8]
+
 	"""
-	start_datetime = datetime.datetime.strptime(start_date, '%Y-%m-%d_%H%M')
-	end_datetime   = datetime.datetime.strptime(end_date,  '%Y-%m-%d_%H%M')
+
+	from dateutil import parser
+	import pandas as pd
+
+	start_datetime = parser.parse(start_date)
+	end_datetime   = parser.parse(end_date)
+	print(start_datetime)
+	print(end_datetime)
 	
 	filenames = [] # create an empty list, ready to create list of *.nc filenames
 	date = start_datetime
-	
+
+	if months == 'all': months=range(1,13)
+
+	### Get level_str from var_name
+	level_str = None
+	df_as = pd.read_csv('/group_workspaces/jasmin/bas_climate/data/ecmwf/era-interim/era-interim_6hrly_surface_vars.csv')
+	df_ap = pd.read_csv('/group_workspaces/jasmin/bas_climate/data/ecmwf/era-interim/era-interim_6hrly_pressure_lev_vars.csv')
+	if var_name in df_as['surface_vars'].values:      level_str = 'as'
+	if var_name in df_ap['pressure_lev_vars'].values: level_str = 'ap'
+	if level_str == None: 
+		print('Surface Vars        =', df_as['surface_vars'].values)
+		print('Pressure Level Vars =', df_ap['pressure_lev_vars'].values)
+		raise ValueError('Can not find files for var_name='+var_name)
+
 	### Get all filenames for all 6 hourly fields between start and end, inclusive
 	while date <= end_datetime:
 		yr       = str(date.year)
@@ -36,9 +61,10 @@ def get_6hr_fnames(start_date, end_date, level_str):
 		minute   = str("{:0>2d}".format(date.minute))
 		date_str = ''.join([yr,mon,day,hr,minute])
 		
-		file = '/badc/ecmwf-era-interim/data/gg/as/'+yr+'/'+mon+'/'+day+'/ggas'+date_str+'.nc'
+		file = '/badc/ecmwf-era-interim/data/gg/'+level_str+'/'+yr+'/'+mon+'/'+day+'/gg'+level_str+''+date_str+'.nc'
 		
-		filenames.append(file)
+		if int(mon) in months:
+			filenames.append(file)
 		
 		# Add 6 hours to read in next file
 		time_increment = datetime.timedelta(days=0, hours=6, minutes=0)
@@ -57,13 +83,18 @@ def edit_erai_attributes(cube, field, filename):
 	cube.coord('t').attributes.pop('time_origin', None)
 
 
-def get_cube(start_date, end_date, level_str, frequency='6hr', constraints=None):
+def get_cube(start_date, end_date, var_name, months='all', frequency='6hr', constraints=None):
 
 	"""
+
 	Get /Path/filenames for ERA-Interim files within date range
-	level_str for JASMIN:
-		'as' surface variables
-		'ap' pressure level variables
+
+	Start and end date can be specified using a range of strings:
+		e.g., '19790101', '1979-01-01', '19790101010101'
+
+	var_name is a constraint on the cube variable name (i.e., cube.var_name)
+		e.g., var_name = 'T2'
+
 	"""
 
 	### To do....
@@ -72,10 +103,18 @@ def get_cube(start_date, end_date, level_str, frequency='6hr', constraints=None)
 	#
 	# change default frequency to monthly
 
-	if frequency == '6hr': fnames = get_6hr_fnames(start_date, end_date, level_str)
+	if frequency == '6hr': fnames = get_6hr_fnames(start_date, end_date, var_name, months=months)
 	
+	con = iris.Constraint(cube_func=lambda cube: cube.var_name == var_name)
+	
+	### Additional constrains (level, time)
+	if (constraints != None): con = con & constraints
+
 	with units.suppress_errors():
-		cubelist = iris.load(fnames, constraints=constraints, callback=edit_erai_attributes)
+		cubelist = iris.load(fnames, constraints=con, callback=edit_erai_attributes)
+
+	### Setup new coord to add to cube (see below)
+	model_coord = iris.coords.AuxCoord('ERA-Interim', long_name='Model', units='no_unit')
 
 	### Fix cubes to all match a reference cube before we can merge
 	ref_cube = cubelist[0]
@@ -84,7 +123,9 @@ def get_cube(start_date, end_date, level_str, frequency='6hr', constraints=None)
 		c.coord('longitude').points        = ref_cube.coord('longitude').points
 		c.coord('latitude').standard_name  = ref_cube.coord('latitude').standard_name
 		c.coord('longitude').standard_name = ref_cube.coord('longitude').standard_name
-		c.coord('t').var_name              = ref_cube.coord('t').var_name
+		c.coord('t').var_name              = 'time' # To match CMIP standard 
+		c.coord('t').long_name             = 'time' #
+		c.add_aux_coord(model_coord)                # Add model name as a scalar coord
 
 	iris.util.unify_time_units(cubelist)
 	cube = cubelist.concatenate_cube()
