c04072ff86e3b969bcc791d2ef6c046fe5a04907
ScottHosking <jshosking@gmail.com>
2017-07-01 21:42:53 +0100

    add sample_data dir if doesn't exist

--- util.py
+++ util.py
@@ -200,6 +200,10 @@ def eg_cubelist():
 	url = "https://www.unidata.ucar.edu/software/netcdf/examples/sresa1b_ncar_ccsm3-example.nc"
 	nc_file = __baspy_path+'/sample_data/sresa1b_ncar_ccsm3-example.nc'
 
+	### Create sample_data folder if it doesn't already exist
+	if not os.path.exists(__baspy_path+'/sample_data'): 
+		os.makedirs(os.path.expanduser(__baspy_path+'/sample_data'))
+
 	if (os.path.isfile(nc_file) == False):
 		import urllib
 		print('Downloading example netcdf file: '+url)
