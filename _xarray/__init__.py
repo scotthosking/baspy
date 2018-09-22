b95112aba493515a617e754a0a148100e29de751
ScottHosking <jshosking@gmail.com>
2018-09-22 19:04:52 +0100

    add xarray open_dataset def using catalogue

--- _xarray/__init__.py
+++ _xarray/__init__.py
@@ -1,4 +1,22 @@
 from . import util
+import xarray as xr
+
 
 eg_Dataset   = util.eg_Dataset
 eg_DataArray = util.eg_DataArray
+
+
+def open_dataset(df, **kwargs):
+
+    ### sanity checks
+    if 'pandas.core.frame.DataFrame' not in str(type(df)):
+        raise ValueError('not a Pandas DataFrame')
+    if len(df) != 1:
+        raise ValueError('Catalogue contains more than one row')
+
+    directory = df['Path'].values[0]+'/'
+    files     = df['DataFiles'].values[0].split('|')
+    files     = [ directory+f for f in files ]
+    ds        = xr.open_mfdataset(files) 
+
+    return ds
